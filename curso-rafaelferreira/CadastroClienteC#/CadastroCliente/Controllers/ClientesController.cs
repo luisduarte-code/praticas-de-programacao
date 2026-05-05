af248b4cfaaa6d66d1422458bd97674eb4bbbc60
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-05 16:56:45 -0300

    estudos

---
+++ curso-rafaelferreira/CadastroClienteC#/CadastroCliente/Controllers/ClientesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CadastroClientes.Models;
+
+namespace CadastroClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("Salvar")]
+        public object Salvar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+              
+            }
+            catch (Exception ex)
+            {
+              
+            }
+            return null;
+        }
+    }
+}
---
+++ curso-rafaelferreira/CadastroClienteC#/CadastroCliente/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿namespace CadastroClientes.Models
+{
+    public class Clientes
+    {
+        public int IdClientes { get; set; }
+        public string Documento { get; set; } 
+        public string Nome { get; set; }
+        public string Sexo { get; set; } 
+        public string Email { get; set; } 
+        public string Telefone { get; set; } 
+        public string Fax { get; set; }
+        public string UF { get; set; }
+
+    }
+}
