15d37877b263962eadba0f51ccada7169823f589
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-06 10:45:10 -0300

    inicioemc#

--- logicaemc#/logicaemcsharp/Program.cs
+++ logicaemc#/logicaemcsharp/Program.cs
@@ -8,14 +8,29 @@ namespace logicaemcsharp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//seria a função principal
         {
-            //tipo e identificador
-            char PrimeiraLetraNome= 'l';
-            string Nome = "luis";
+            //declarando e atribuindo valores as variaveis
+            char letra;
+            string nome;
+            int n;
+            float nf;
+            double na;
+            bool nsei;
 
-            Console.WriteLine(PrimeiraLetraNome);
-            Console.WriteLine(Nome);
+            letra='K';
+            nome="Luis";
+            n=1;
+            nf=1.5f;
+            na=300000;
+            nsei=false;
+
+            Console.WriteLine(letra);
+            Console.WriteLine(nome);
+            Console.WriteLine(n);
+            Console.WriteLine(nf);
+            Console .WriteLine(na);
+            Console.WriteLine(nsei);
         }
     }
 }
