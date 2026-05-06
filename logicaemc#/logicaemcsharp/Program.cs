1d523e9e14b39abfd9432f15e778e97325bfb10e
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-06 00:10:37 -0300

    estudos

---
+++ logicaemc#/logicaemcsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicaemcsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo e identificador
+            char PrimeiraLetraNome= 'l';
+            string Nome = "luis";
+
+            Console.WriteLine(PrimeiraLetraNome);
+            Console.WriteLine(Nome);
+        }
+    }
+}
