91db9a0020894d2153967c7c74a4fca651be6abd
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-20 11:06:28 -0300

    Create somaarray.c

---
+++ logicaemc/funcoes/somaarray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int somaArray(int array[], int tamanho);
+int main(){
+	int tamanho;
+	printf("Informe o tamanho do array: ");
+	scanf("%d",&tamanho);
+	int array[tamanho];
+	printf("A soma do array deu: %d", somaArray(array,tamanho));
+	return 0;
+}
+int somaArray(int array[], int tamanho){
+	int somaArray = 0;
+	for(int i=0;i<tamanho;i++){
+		printf("Informe o %d numero:",i);
+		scanf("%d",&array[i]);
+		somaArray = somaArray + array[i];
+	}
+	return somaArray;
+}
