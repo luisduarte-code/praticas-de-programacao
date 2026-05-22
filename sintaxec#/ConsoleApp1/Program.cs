ebe8b3daeb3894025b343e64138fa3b9b5fb9cfd
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-22 19:02:05 -0300

    estudo

---
+++ sintaxec#/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valor = 3F / 2F;
+            System.Console.WriteLine(valor);
+            Console.ReadLine();
+
+        }
+    }
+}
