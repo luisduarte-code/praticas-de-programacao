453cd6e34ff0faa0b9f6fac2ecf67d2976068c6f
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-26 10:53:33 -0300

    .

--- sintaxec#/ConsoleApp1/Program.cs
+++ sintaxec#/ConsoleApp1/Program.cs
@@ -9,11 +9,18 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void estoquedepecas(string[] args)
         {
-           int x= Convert.ToInt32(Console.ReadLine());
-           
-           
+         //inserir a qnt minima de peças que essa peça deve de ter no estoque 
+            Console.WriteLine("Infrome a quantidade minima de peças que precisa ter no estoque: ");
+            int qmin = Convert.ToInt32(Console.ReadLine());
+        //qtd maxima
+            Console.WriteLine("Infrome a quantidade minima de peças que precisa ter no estoque: ");
+            int qmax = Convert.ToInt32(Console.ReadLine());
+        //calculo da media de peças
+            int em = (qmin + qmax) / 2;
+        //resultado
+            Console.WriteLine($"O estoque minimo é de: {em}");
         }
     }
 }
---
+++ sintaxec#/ConsoleApp1/comissao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class comissao
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do vendedor: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe o código da peça: ");
+            int cp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("informe o valor da peça: ");
+            float vp = float.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade de peças vendidas: ");
+            int qtdsell = Convert.ToInt32(Console.ReadLine());
+            float cvendedor = (vp * qtdsell) * 0.05f;
+            Console.WriteLine($"A comissao do vender sera de: {cvendedor}");
+        }
+
+    }
+}
