9ac9908929c83ad3ae3c10961eba02900447e716
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-28 00:05:42 -0300

    .

--- sintaxec#/matrizes/Program.cs
+++ sintaxec#/matrizes/Program.cs
@@ -11,14 +11,12 @@ namespace matrizes
     {
         static void Main(string[] args)
         {
-            // declaração de uma matriz
-            int i = 0;
-            int j=0;
-            int soma = 0;
+            // declaração de uma matriz e demais variaveis
+            int i = 0, j = 0, soma = 0;
             int[,] num = new int[3, 3];
-            for ( i = 0; i < num.GetLength(0); i++)
+            for ( i = 0; i < num.GetLength(0); i++)//percorre linha
             {
-                for ( j = 0; j < num.GetLength(1); j++)
+                for ( j = 0; j < num.GetLength(1); j++)//percorre coluna
                 {
                     Console.WriteLine($"Informe o numero da posição [{i}][{j}] ");
 
@@ -26,13 +24,14 @@ namespace matrizes
                 }
             }
             //para a soma total
-            for (i=0; i < num.GetLength(0); i++)
+            for (i=0; i < num.GetLength(0); i++)//funcao que retorna o tamanho da linha
             {
-                for(j=0;j < num.GetLength(1); j++)
+                for(j=0;j < num.GetLength(1); j++)// colocando o 1 na frente retorna o tamanho da coluna
                 {
                     soma += num[i, j];
                 }
             }
+            //retorna a soma total
             Console.WriteLine($"A soma da matriz é= {soma}");
         }
     }
