b5d4b95ee021c20074c7ce302dee3ffd5cd54858
Luís Duarte <luissoarespessoal@gmail.com>
2026-05-29 10:32:12 -0300

    .

---
+++ fundamentos-csharp/metodos_funcoes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodos_funcoes
+{
+    internal class Program
+    {
+        
+        public static int soma(int x, int y)
+        {
+            return x + y;
+        }
+    
+        //n pertence a uma classe     Tipo de retorno que é void              Nome do método       Parametros
+        static                        void                                    Main                (string[] args)
+        {
+        //procedimentos nao retornam nada e funcoes retornam alguma coisa
+            Console.WriteLine("Informe o x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A soma é = {soma (x,y)}");
+        }
+    }
+}
